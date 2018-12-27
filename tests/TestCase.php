8e7705f6be22e90d4265e1aefc582817bfd6acc0
Josh Pollock <josh@calderawp.com>
2018-12-27 17:02:47 -0500

    make core a package so I do not keep losing the autolaoder for core

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,7 +6,7 @@ namespace calderawp\caldera\restApi\Tests;
 use calderawp\caldera\core\CalderaCore;
 use calderawp\caldera\Events\CalderaEvents;
 use calderawp\caldera\restApi\CalderaRestApi;
-use calderawp\caldera\Tests\Traits\SharedFactories;
+use calderawp\caldera\Core\Tests\Traits\SharedFactories;
 use calderawp\CalderaContainers\Service\Container;
 
 abstract class TestCase extends \Mockery\Adapter\Phpunit\MockeryTestCase
