e96aa60f26b6e660675f32ac2d1eedf15a1743f3
Josh Pollock <josh@calderawp.com>
2018-12-27 13:26:10 -0500

    entry endpoints -- simpler than forms

---
+++ src/Endpoints/Entry/GetEntries.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace calderawp\caldera\restApi\Endpoints\Entry;
+
+
+class GetEntries
+{
+
+}
---
+++ tests/Endpoints/Entry/GetEntriesTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace calderawp\caldera\restApi\Tests\Endpoints\Entry;
+
+use calderawp\caldera\restApi\Endpoints\Entry\GetEntries;
+use PHPUnit\Framework\TestCase;
+
+class GetEntriesTest extends TestCase
+{
+
+}
