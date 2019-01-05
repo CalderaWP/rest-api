0c1d7e1d1feb5c68ed403931b0706d69c83d2c01
Josh Pollock <josh@calderawp.com>
2019-01-05 17:30:57 -0500

    move Request class to Http package

--- src/Request.php
+++ src/Request.php
@@ -3,28 +3,4 @@
 
 namespace calderawp\caldera\restApi;
 
-use calderawp\interop\Contracts\Rest\RestRequestContract;
-use calderawp\interop\Traits\Rest\ProvidesHttpHeaders;
-use calderawp\interop\Traits\Rest\ProvidesRestParams;
-
-class Request implements RestRequestContract
-{
-
-	use ProvidesHttpHeaders, ProvidesRestParams;
-
-
-	public static function fromArray(array $items = ['headers' => [], 'params' => [] ])
-	{
-		$obj = new static();
-		if (! empty($items['headers'])) {
-			foreach ($items['headers'] as $header => $value) {
-				$obj->setHeader($header, $value);
-			}
-		}
-
-		if (! empty($items['params'])) {
-			$obj->setParams($items['params']);
-		}
-		return $obj;
-	}
-}
+class Request extends \calderawp\caldera\Http\Request {};
